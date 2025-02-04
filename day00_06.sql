295741516f9c30cadbffff55f2cb09587826eb82
SemperFidel <zandrekh.40000@gmail.com>
2025-02-04 14:59:13 +0300

    Update day00_06.sql

--- day00_06.sql
+++ day00_06.sql
@@ -1,1 +1,8 @@
-
+SELECT DISTINCT
+    (SELECT name FROM person WHERE id = person_order.person_id LIMIT 1) AS NAME,
+    CASE
+        WHEN (SELECT name FROM person WHERE id = person_order.person_id LIMIT 1) = 'Denis' THEN TRUE
+        ELSE FALSE
+END AS check_name
+FROM person_order
+WHERE menu_id IN (13, 14, 18) AND order_date = '2022-01-07'
