341406d22f2bff204870eb01c19eecea256948e4
SemperFidel <zandrekh.40000@gmail.com>
2025-02-04 14:22:51 +0300

    Update day01_01.sql

--- day01_01.sql
+++ day01_01.sql
@@ -1,3 +1,3 @@
-SELECT id AS object_id, pizza_name AS object_name FROM menu
-UNION ALl SELECT id AS object_id, name as object_name FROM person
-ORDER BY object_id
+SELECT pizza_name AS object_name FROM menu
+UNION ALl SELECT name as object_name FROM person
+ORDER BY object_name
