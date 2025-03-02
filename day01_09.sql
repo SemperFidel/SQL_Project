9657318e6de08d5fb69a0794996a71170d7c6538
SemperFidel <zandrekh.40000@gmail.com>
2025-03-02 23:31:16 +0300

    Create day01_09.sql

---
+++ day01_09.sql
@@ -0,0 +1,6 @@
+SELECT name FROM pizzeria
+WHERE id NOT IN (SELECT DISTINCT pizzeria_id FROM person_visits);
+
+SELECT name FROM pizzeria pz
+WHERE NOT EXISTS (SELECT 1 FROM person_visits pv
+                           WHERE pv.pizzeria_id  = pz.id);
