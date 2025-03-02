73a322b44436490b4be947e3d30dde7dc42333c9
SemperFidel <zandrekh.40000@gmail.com>
2025-03-02 21:36:49 +0300

    Update day01_06.sql

--- day01_06.sql
+++ day01_06.sql
@@ -1,1 +1,6 @@
-
+SELECT p.name AS person_name, po.order_date AS action_date FROM person_order po
+    JOIN person p ON po.person_id = p.id
+UNION
+SELECT p.name AS person_name, pv.visit_date AS action_date FROM person_visits pv
+    JOIN person p ON pv.person_id = p.id
+ORDER BY action_date ASC, person_name DESC
